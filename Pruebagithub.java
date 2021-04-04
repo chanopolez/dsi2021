f111cd11d54fcd21782db334aa3f9ded0357f590
chanopolez <81364616+chanopolez@users.noreply.github.com>
2021-04-04 17:13:45 -0300

    Add files via upload

---
+++ Pruebagithub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagithub;
+
+/**
+ *
+ * @author nacho
+ */
+public class Pruebagithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=4,b=7,res=a+b;
+        
+        System.out.println("La suma entre 4 y 7 da "+res);
+    }
+    
+}
