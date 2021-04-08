36be8573926242cb4b0938c40ff1014d734bc94c
chanopolez <81364616+chanopolez@users.noreply.github.com>
2021-04-08 09:21:59 -0300

    Add files via upload

---
+++ Condicional3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicional3;
+
+/**
+ *
+ * @author pc15
+ */
+public class Condicional3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Números del 1 al 100: ");
+        for(int i=1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
